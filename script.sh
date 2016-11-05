0198660c4eebaef6c39c5e6e28df5ae27de7844e
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-05 17:30:43 +1300

    remove eslint

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-echo '-- run eslint --'
-eslint .
-
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
     echo '-- build production --'
