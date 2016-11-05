696e9dd5650b77dccabf8f7b29626237c664548a
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-11-05 18:48:15 +1300

    remove out and minification for now

--- webpack.make.js
+++ webpack.make.js
@@ -282,15 +282,15 @@ module.exports = function makeWebpackConfig(options) {
         config.plugins.push(
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
             // Only emit files when there are no errors
-            new webpack.NoErrorsPlugin(),
+            // new webpack.NoErrorsPlugin(),
 
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
             // Dedupe modules in the output
-            new webpack.optimize.DedupePlugin(),
+            // new webpack.optimize.DedupePlugin(),
 
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
             // Minify all javascript, switch loaders to minimizing mode
-            new webpack.optimize.UglifyJsPlugin()
+            // new webpack.optimize.UglifyJsPlugin()
         );
     }
 
